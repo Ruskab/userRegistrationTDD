45e09b02a02702bf502544cb301fe2b7b861c3ce
ruskab <ilya.kab@gmail.com>
2021-08-05 20:49:11 +0200

    rename to extract

--- src/test/java/ikab/tdd/tdduserregistration/FakeRegistrationServiceContract.java
+++ src/test/java/ikab/tdd/tdduserregistration/FakeRegistrationServiceContract.java
@@ -10,7 +10,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class RegistrationServiceContract {
+public class FakeRegistrationServiceContract {
 
     @Test
     public void createUser() {
