89905cd7563a33fd37be901d357e969933256511
ruskab <ilya.kab@gmail.com>
2021-08-05 19:05:54 +0200

    init commit

---
+++ src/main/java/ikab/tdd/tdduserregistration/TddUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ikab.tdd.tdduserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddUserRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddUserRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ikab/tdd/tdduserregistration/TddUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ikab.tdd.tdduserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddUserRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
